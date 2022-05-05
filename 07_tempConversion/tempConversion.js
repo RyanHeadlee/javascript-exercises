ffc6c6aa5339592a02d9844be8865759c2e2a98e
Ryan Headlee <headleeryan@gmail.com>
2022-05-05 15:34:52 -0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+  let result = (degrees - 32) * (5/9);
+  if (result === Math.round(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(degrees) {
+  let result = degrees * (9/5) + 32;
+  if (result === Math.round(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
