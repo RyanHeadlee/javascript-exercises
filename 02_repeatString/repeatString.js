20d27342cc3de92b99cc9765f11a670005b9dc42
Ryan Headlee <headleeryan@gmail.com>
2022-05-05 10:02:54 -0700

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+let results;
 
+const repeatString = function(string, num) {
+  if (num === 1) {
+    return string;
+  } else if (num === 0) {
+    return "";
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < (num - 1); i++) {
+      if (i === 0) {
+        results = string + string;
+      } else {
+        results = results + string;
+      }
+    }
+  }
+  return results;
 };
 
 // Do not edit below this line
