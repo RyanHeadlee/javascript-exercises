93227ae6e03c9d7350bbf866a7ee77b065426505
Ryan Headlee <headleeryan@gmail.com>
2022-05-05 11:36:19 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+  if (args.length === 0) {
+    return myArray;
+  } else if (args.length > 0) {
+    let forDeletion = [args[0], args[1], ...args];
+    return myArray.filter(item => !forDeletion.includes(item));
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
