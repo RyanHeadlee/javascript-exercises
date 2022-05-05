03545f9d729e3c077d83b0ce6f7de703d56d511e
Ryan Headlee <headleeryan@gmail.com>
2022-05-05 13:21:21 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,38 @@
-const sumAll = function() {
-
+const sumAll = function(range1, range2) {
+  let x;
+  let sum;
+  let doOnce = true;
+  if (range1 < 0 || range2 < 0 || typeof(range1) !== "number" || typeof(range2) !== "number") {
+    return "ERROR";
+  } else if (range1 < range2) {  
+    for (range1; range1 < range2; range1++) {
+      if (doOnce === true) {
+        x = range1 + 1;
+        sum = range1 + x;
+        x++;
+        doOnce = false;
+      } else {
+        x;
+        sum = sum + x;
+        x++;
+      }
+    }
+    return sum;
+  } else if (range1 > range2) {
+    for (range2; range2 < range1; range2++) {
+      if (doOnce === true) {
+        x = range2 +1;
+        sum = range2 + x;
+        x++;
+        doOnce = false;
+      } else {
+        x;
+        sum = sum + x;
+        x++;
+      }
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
